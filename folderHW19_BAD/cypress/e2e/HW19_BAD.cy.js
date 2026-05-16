808a5e3b0e1273b4507b40371917241b0db8a5c1
monovita <depointon@gmail.com>
2026-05-16 23:35:46 +0300

    Updated folderHW19_BAD and folder19_GOOD with adding headers links

--- folderHW19_BAD/cypress/e2e/HW19_BAD.cy.js
+++ folderHW19_BAD/cypress/e2e/HW19_BAD.cy.js
@@ -9,9 +9,35 @@ describe ("Links", () => {
   })
 
     it ("Sign Up button", () => {
-      cy.get(`[class="hero-descriptor_btn btn btn-primary"]`).click()
+      cy.get(`[class="hero-descriptor_btn btn btn-primary"]`).click();
       cy.get(`[class="close"]`).click();
     });
+
+    it ("hillelauto link", () => {
+      cy.get(`[class="header_logo"]`).click();
+    })
+
+    it ("Home link", () => {
+      cy.get(`[class="btn header-link -active"]`).click();
+    })
+
+    it ("About link", () => {
+      cy.get(`[appscrollto="aboutSection"]`).click();
+    })
+
+    it ("Contacts link", () => {
+      cy.get(`[appscrollto="contactsSection"]`).click();
+    })
+
+    it ("Guest log in button", () => {
+      cy.get(`[class="header-link -guest"]`).click();
+      cy.go('back');
+    })
+
+    it ("Log in button", () => {
+      cy.get(`[class="btn btn-outline-white header_signin"]`).click();
+      cy.get(`[class="close"]`).click();
+    })
 
     it ("Facebook link", () => {
       cy.get(`[class="socials_icon icon icon-facebook"]`).click();
--- folderHW19_GOOD/cypress/e2e/HW19_GOOD.cy.js
+++ folderHW19_GOOD/cypress/e2e/HW19_GOOD.cy.js
@@ -1,14 +1,14 @@
 import { MainPage } from "../helpers/Page Objects/mainPage";
 import { SocialLinks } from "../helpers/Page Objects/socialLinks";
 import { ContactLinks } from "../helpers/Page Objects/contactLinks";
+import { HeaderLinks } from "../helpers/Page Objects/headerLinks";
 import { Credentials } from "../helpers/Page Objects/credentials";
-
-
 
 describe ("Links of qauto.forstudy.space", () => {
   const mainPage = new MainPage();
   const socialLinks = new SocialLinks();
   const contactLinks = new ContactLinks();
+  const headerLinks = new HeaderLinks();
   const credentials = new Credentials();
 
   beforeEach(() => {
@@ -26,7 +26,48 @@ describe ("Links of qauto.forstudy.space", () => {
       mainPage.closeSignUp.click();
     });
 
-    it ("Facebook link", () => {
+    it("hillelauto link", () => {
+      headerLinks.hillelauto
+      .should("be.visible")
+      .and("not.be.disabled")
+      .click();
+    });
+
+    it("Home link", () => {
+      headerLinks.home
+      .should("be.visible")
+      .and("not.be.disabled")
+      .click();
+    })
+
+    it("About link", () => {
+      headerLinks.about
+      .should("be.visible")
+      .and("not.be.disabled")
+      .click();
+    })
+
+    it("Contacts link", () => {
+      headerLinks.contacts
+      .should("be.visible")
+      .and("not.be.disabled")
+      .click();
+    })
+
+    it("Guest Log In", () => {
+      headerLinks.guestLogIn
+      .should("be.visible")
+      .and("not.be.disabled")
+      .click();
+    })
+
+    it("Log In", () => {
+      headerLinks.logIn.click();
+      headerLinks.logInModal.should("be.visible");
+      headerLinks.closeLogIn.click();
+    })
+
+    it("Facebook link", () => {
       socialLinks.facebook
       .should("be.visible")
       .and("not.be.disabled")
